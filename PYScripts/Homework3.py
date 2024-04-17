f7834815c24639a6954438134656a16ee16d9ffa
nickwntr <nicolawntr@gmail.com>
2024-04-17 19:08:15 +0300

    HW

---
+++ PYScripts/Homework3.py
@@ -0,0 +1,8 @@
+mystring = input()
+#Lorem ipsum sir
+print(len(mystring))
+print(mystring.lower())
+print(mystring.upper())
+print(mystring.replace(" ",""))
+print(mystring[0])
+print(mystring[len(mystring)-1])
