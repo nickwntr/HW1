1fb3bf4320004a6288ee7f9096b577093932bd18
nickwntr <94203954+nickwntr@users.noreply.github.com>
2024-08-30 18:52:33 +0300

    Update main.py

--- PYScripts/test2.4/main.py
+++ PYScripts/test2.4/main.py
@@ -4,11 +4,11 @@ for i in range(15):
 primes = []
 noprimes = []
 for i in numbers:
-    if (i % 10 == 0) or (i % 5 == 0) or (i % 3 == 0) or (i % 2 == 0):
+    if ((i % 10 == 0) and (i / 10 > 1)) or ((i % 5 == 0) and (i / 5 > 1)) or ((i % 3 == 0) and (i / 3 > 1)) or ((i % 2 == 0) and (i / 2 > 1)):
         noprimes.append(i)
     else:
         primes.append(i)
-for i in primes:
-    print(i)
-for i in noprimes:
-    print(i)
+print("Primes: ")
+print(primes)
+print("Not Primes: ")
+print(noprimes)
