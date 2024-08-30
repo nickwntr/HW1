d6ae8a3b6fbe57c40b83dfdcabacb47e85e286b1
nickwntr <94203954+nickwntr@users.noreply.github.com>
2024-08-30 19:17:45 +0300

    Update main.py

--- PYScripts/test2.5/main.py
+++ PYScripts/test2.5/main.py
@@ -1,17 +1,29 @@
 from random import *
-def get_matrix(x,y,values=0):
+def get_matrix(x,y,values):
     matrix = []
     rand = False
-    if values == 0: rand = True
+    if len(values) == 0: rand = True
     for i in range(x):
         addrow = []
         for j in range(y):
             if rand == True:
                 addrow.append(randint(1,100))
             else:
-                addrow.append(values)
+                addrow.append(values[randint(0, len(values)-1)])
         matrix.append(addrow)
     for i in matrix:
         print(i)
+print("Ведите длину, ширину массива, и его данные. Набор данных окончить END")
+x = int(input())
+y = int(input())
+values = []
+minusoneterm = True
+while minusoneterm:
+    inp = input()
+    if inp.lower() == "END".lower():
+        minusoneterm = False
+        break
+    else:
+        values.append(inp)
 
-get_matrix(int(input()),int(input()),0)
+get_matrix(x, y, values)
